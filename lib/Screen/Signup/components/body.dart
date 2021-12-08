d3297272487eafe2fbe4cc085fef1cbb6a670f49
Enam1997 <89854906+Enam1997@users.noreply.github.com>
2021-12-09 04:59:59 +0800

    Add SignUp Method

--- lib/Screen/Signup/components/body.dart
+++ lib/Screen/Signup/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:auction_app/Screen/Home/home_screen.dart';
 import 'package:auction_app/Screen/Login/login_screen.dart';
 import 'package:auction_app/components/already_have_an_account_acheck.dart';
 import 'package:auction_app/components/or_divider.dart';
@@ -5,12 +6,15 @@ import 'package:auction_app/components/rounded_button.dart';
 import 'package:auction_app/components/rounded_input_field.dart';
 import 'package:auction_app/components/rounded_password_field.dart';
 import 'package:auction_app/components/social_icon.dart';
+import 'package:auction_app/utils/authentication.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import 'background.dart';
 
 class Body extends StatelessWidget {
+  bool _isSigningIn = false;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -63,7 +67,25 @@ class Body extends StatelessWidget {
                   ),
                   SocalIcon(
                     iconSrc: "assets/icons/google-plus.svg",
-                    press: () {},
+                      press: () async {
+                        _isSigningIn = true;
+
+
+                        User? user =
+                        await Authentication.signInWithGoogle(context: context);
+
+
+                        _isSigningIn = false;
+                        ;
+
+                        if (user != null) {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (context) => HomeScreen(),
+                            ),
+                          );
+                        }
+                      },
                   ),
                   SocalIcon(
                     iconSrc: "assets/icons/twitter.svg",
